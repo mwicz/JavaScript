16113f6bc2d27fc1423bfb8542928196a6846a81
MariaPysklevicz <wicz_c3@hotmail.com>
2022-08-03 16:34:59 +0900

    Create ambiente.js

---
+++ 05/ambiente.js
@@ -0,0 +1,21 @@
+// estruturas de repetições
+for(var c = 1; c<=5; c++){
+    console.log(c)
+}
+
+
+
+/*
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+}while(c <= 10)
+*/
+/*
+var c = 1
+while(c <= 6){
+    console.log(`passo ${c}`)
+    c++
+}
+ */
